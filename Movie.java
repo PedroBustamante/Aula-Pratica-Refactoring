9c57d9edc69a807878de8ec56f27747485a1a464
PedroBustamante <pedro.rios.bust@gmail.com>
2021-03-12 19:51:12 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -38,9 +38,7 @@ public class Movie {
         return _price.getCharge(daysRented);
     }
     public int getFrequentRenterPoints(int daysRented){
-       if ((this.getPriceCode() == NEW_RELEASE) &&
-                daysRented > 1) return 2;
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
    }
     
 }
